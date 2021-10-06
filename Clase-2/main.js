90b6bf056e458f7d6140a41849d281270557dcfc
Emilio Gril <emiliogril@gmail.com>
2021-10-06 17:43:15 -0300

    Initial commit

--- Clase-2/main.js
+++ Clase-2/main.js
@@ -4,7 +4,7 @@ let numero1 = parseInt(prompt("Escribí un número:"));
 console.log (numero1); 
 
 if (numero1 <= 100 && !80 && !50 ) {
-        alert("Has escrito un número igual o menor a 100 😎");
+    alert("Has escrito un número igual o menor a 100 😎");
 }     
 
 else if (numero1 === 80) {    
@@ -15,14 +15,15 @@ else if (numero1 === 50) {    
     alert("Has escrito un número igual a 50");
 
 }
-else {    alert("Has escrito un número mayor a 100");
+else {
+    alert("Has escrito un número mayor a 100");
 }
 
 //Utilizo prompts para pedir un número y si esta entre 10 y 50 muestro alert:
 let solicitoNumero = parseFloat(prompt("Escriba un número:"));
 if (solicitoNumero >=10 && solicitoNumero <=50) { 
-       alert("Tu número ronda entre los 10 y 50");
+    alert("Tu número ronda entre los 10 y 50");
 }
- else {    
+else {    
     console.log ("Se eligió el número: ",solicitoNumero);
 } 
---
+++ Clase-3/main.js
@@ -0,0 +1,12 @@
+
+let number1 = parseInt(prompt('CALCULADORA \nIngrese el primer numero para sumar \nSolo se podra hacer una operacion'))
+
+for (let i = 0; i <= 0; i++) {
+    let number2 = parseInt(prompt('CALCULADORA \nIngrese el segundo numero para sumar \nSolo se podra hacer una operacion'))
+    let suma = number1 + number2;
+    alert('Su resultado es: ' + suma);
+}
+
+
+
+
